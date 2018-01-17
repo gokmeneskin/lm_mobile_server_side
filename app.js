194b8b7759ccc5ccfc831f273bcb31bf313399b2
gokmeneskin <gokmeneskin@gmail.com>
2018-01-17 13:44:43 +0300

    CurrentShiftEvents ikinci test

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const users = require('./routers/users');
 app.use('/api/users', users);
 const clientServers = require('./routers/clientServers');
 app.use('/api/clientservers', clientServers);
+const currentShiftEvents = require('./routers/currentShiftEvents');
+app.use('/api/currentshiftevents', currentShiftEvents);
 
 app.listen(port, () => {
    console.log('Server ' + port + ' port üzerinden başlatıldı');
